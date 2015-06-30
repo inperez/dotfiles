96c0d6d4ebfae183fc9b208e1686e6e07e4db9c4
Martin Bravo <bravomartin@gmail.com>
2015-06-30 09:39:29 -0400

    add tab.bash

---
+++ tab.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Open new Terminal tabs from the command line
+#
+# Author: Justin Hileman (http://justinhileman.com)
+#
+# Installation:
+#     Add the following function to your `.bashrc` or `.bash_profile`,
+#     or save it somewhere (e.g. `~/.tab.bash`) and source it in `.bashrc`
+#
+# Usage:
+#     tab                   Opens the current directory in a new tab
+#     tab [PATH]            Open PATH in a new tab
+#     tab [CMD]             Open a new tab and execute CMD
+#     tab [PATH] [CMD] ...  You can prob'ly guess
+
+# Only for teh Mac users
+[ `uname -s` != "Darwin" ] && return
+
+function tab () {
+    local cmd=""
+    local cdto="$PWD"
+    local args="$@"
+
+    if [ -d "$1" ]; then
+        cdto=`cd "$1"; pwd`
+        args="${@:2}"
+    fi
+
+    if [ -n "$args" ]; then
+        cmd="; $args"
+    fi
+    goto=$cdto\$cmd
+    osascript -e "tell application \"Terminal\"" \
+        -e "do script \"clear\" in front window" \
+        -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+        -e "do script \"cd $goto; clear\" in front window" \
+        -e "end tell"
+        > /dev/null
+}
